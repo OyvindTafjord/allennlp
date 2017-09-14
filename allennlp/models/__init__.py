8c9fb5406e227cc90512c454ac6ff6e97a0e3694
Oyvind Tafjord <OyvindT@allenai.org>
2017-09-14 14:08:44 -0700

    messing around with nospan BiDAF

--- allennlp/models/__init__.py
+++ allennlp/models/__init__.py
@@ -5,6 +5,7 @@ all of which are subclasses of :class:`~allennlp.models.model.Model`.
 
 from allennlp.models.archival import archive_model, load_archive
 from allennlp.models.bidaf import BidirectionalAttentionFlow
+from allennlp.models.bidaf_nospan import BidirectionalAttentionFlowNoSpan
 from allennlp.models.decomposable_attention import DecomposableAttention
 from allennlp.models.model import Model
 from allennlp.models.simple_tagger import SimpleTagger
