40daf3d1f3398892354a0ed4ca7fec62f6054a7f
Oyvind Tafjord <OyvindT@allenai.org>
2018-05-10 10:53:55 -0700

    Tie action embedding in/out, add denotation accuracy

--- allennlp/semparse/worlds/friction_world.py
+++ allennlp/semparse/worlds/friction_world.py
@@ -10,8 +10,7 @@ from nltk.sem.logic import Type
 from overrides import overrides
 import pyparsing
 
-from allennlp.common import util
-from allennlp.common.util import JsonDict
+from allennlp.semparse import util as semparse_util
 from allennlp.semparse.type_declarations import friction_type_declaration as types
 from allennlp.semparse.worlds.world import World
 from allennlp.semparse.contexts import TableQuestionKnowledgeGraph
@@ -72,3 +71,40 @@ class FrictionWorld(World):
         Returns ``True`` if the given entity is one of the entities in the table.
         """
         return False
+
+    # Simple table for how attributes relates qualitatively to friction
+    qr_coeff = {
+        "friction": 1,
+        "smoothness": -1,
+        "speed": -1,
+        "distance": -1,
+        "heat": 1
+    }
+
+    @staticmethod
+    def _exec_infer(setup, *answers):
+        answer_index = -1
+        for index, answer in enumerate(answers):
+            attribute_dir = FrictionWorld.qr_coeff[setup[0]] * FrictionWorld.qr_coeff[answer[0]]
+            change_same = 1 if setup[1] == answer[1] else -1
+            world_same = 1 if setup[2] == answer[2] else -1
+            if attribute_dir * change_same * world_same == 1:
+                if answer_index > -1:
+                    # Found two valid answers
+                    answer_index = -2
+                else:
+                    answer_index = index
+        return answer_index
+
+    @staticmethod
+    def execute(lf: str) -> int:
+        """
+        Very basic model for executing friction logical forms. For now returns answer index (or
+        -1 if no answer can be concluded)
+        """
+        parse = semparse_util.lisp_to_nested_expression(lf)
+        if len(parse) < 1 and len(parse[0]) < 2:
+            return -1
+        if parse[0][0] == 'infer':
+            return FrictionWorld._exec_infer(*parse[0][1:])
+        return -1
