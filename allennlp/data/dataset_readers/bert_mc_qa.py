09abb8a151589cee2e86ebc8ee3773b33b5add01
Oyvind Tafjord <OyvindT@allenai.org>
2019-06-08 12:45:48 -0700

    Initial annotation tagging code

--- allennlp/data/dataset_readers/bert_mc_qa.py
+++ allennlp/data/dataset_readers/bert_mc_qa.py
@@ -18,31 +18,14 @@ from allennlp.data.tokenizers import Token, Tokenizer, WordTokenizer
 from allennlp.data.tokenizers.word_splitter import WordpieceWordSplitter
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
-
+# TagSpanType = ((int, int), str)
 
 @DatasetReader.register("bert_mc_qa")
 class BertMCQAReader(DatasetReader):
     """
-    Reads a file from the AllenAI-V1-Feb2018 dataset in Json format.  This data is
-    formatted as jsonl, one json-formatted instance per line.  An example of the json in the data is:
-
-        {"id":"MCAS_2000_4_6",
-        "question":{"stem":"Which technology was developed most recently?",
-            "choices":[
-                {"text":"cellular telephone","label":"A"},
-                {"text":"television","label":"B"},
-                {"text":"refrigerator","label":"C"},
-                {"text":"airplane","label":"D"}
-            ]},
-        "answerKey":"A"
-        }
 
     Parameters
     ----------
-    tokenizer : ``Tokenizer``, optional (default=``WordTokenizer()``)
-        We use this ``Tokenizer`` for both the premise and the hypothesis.  See :class:`Tokenizer`.
-    token_indexers : ``Dict[str, TokenIndexer]``, optional (default=``{"tokens": SingleIdTokenIndexer()}``)
-        We similarly use this for both the premise and the hypothesis.  See :class:`TokenIndexer`.
     """
 
     def __init__(self,
@@ -56,6 +39,7 @@ class BertMCQAReader(DatasetReader):
                  skip_id_regex: str = None,
                  ignore_context: bool = False,
                  skip_and_offset: List[int] = None,
+                 annotation_tags: List[str] = None,
                  context_strip_sep: str = None,
                  context_syntax: str = "c#q#a",
                  sample: int = -1) -> None:
@@ -75,6 +59,11 @@ class BertMCQAReader(DatasetReader):
         self._ignore_context = ignore_context
         self._skip_and_offset = skip_and_offset
         self._context_strip_sep = context_strip_sep
+        self._annotation_tags = annotation_tags
+        if self._annotation_tags is not None:
+            self._tokenizer = WordTokenizer()
+            self._num_annotation_tags = len(self._annotation_tags)
+            self._annotation_tag_index = {tag:idx for idx, tag in enumerate(self._annotation_tags)}
 
 
     @overrides
@@ -120,7 +109,15 @@ class BertMCQAReader(DatasetReader):
                 context = item_json.get("para")
                 if self._ignore_context:
                     context = None
+                context_annotations = None
+                if context is not None and self._annotation_tags is not None:
+                    para_tagging = item_json.get("para_tagging")
+                    context_annotations = self._get_tagged_spans(para_tagging)
                 question_text = item_json["question"]["stem"]
+                question_tagging = item_json.get("question_tagging")
+                question_stem_annotations = None
+                if question_tagging is not None and self._annotation_tags is not None:
+                    question_stem_annotations = self._get_tagged_spans(question_tagging.get("stem"))
 
                 if self._context_strip_sep is not None and context is not None:
                     split = context.split(self._context_strip_sep, 1)
@@ -136,9 +133,14 @@ class BertMCQAReader(DatasetReader):
                 choice_label_to_id = {}
                 choice_text_list = []
                 choice_context_list = []
+                choice_label_list = []
+                choice_annotations_list = []
 
                 any_correct = False
                 choice_id_correction = 0
+                choice_tagging = None
+                if question_tagging is not None and self._annotation_tags is not None:
+                    choice_tagging = question_tagging['choices']
 
                 for choice_id, choice_item in enumerate(item_json["question"]["choices"]):
 
@@ -158,8 +160,14 @@ class BertMCQAReader(DatasetReader):
                     if self._ignore_context:
                         choice_context = None
 
+                    choice_annotations = []
+                    if choice_tagging is not None and self._annotation_tags is not None:
+                        choice_annotations = self._get_tagged_spans(choice_tagging[choice_label])
+
                     choice_text_list.append(choice_text)
                     choice_context_list.append(choice_context)
+                    choice_label_list.append(choice_label)
+                    choice_annotations_list.append(choice_annotations)
 
                     is_correct = 0
                     if item_json.get('answerKey') == choice_label:
@@ -197,13 +205,16 @@ class BertMCQAReader(DatasetReader):
                             continue
 
                     yield self.text_to_instance(
-                        item_id,
-                        question_text,
-                        choice_text_list,
-                        answer_id,
-                        context,
-                        choice_context_list,
-                        debug)
+                        item_id=item_id,
+                        question=question_text,
+                        choice_list=choice_text_list,
+                        answer_id=answer_id,
+                        context=context,
+                        choice_context_list=choice_context_list,
+                        context_annotations=context_annotations,
+                        question_stem_annotations=question_stem_annotations,
+                        choice_annotations_list=choice_annotations_list,
+                        debug=debug)
 
 
     def text_to_instance_per_choice(self,  # type: ignore
@@ -251,6 +262,9 @@ class BertMCQAReader(DatasetReader):
                          answer_id: int = None,
                          context: str = None,
                          choice_context_list: List[str] = None,
+                         context_annotations = None,
+                         question_stem_annotations = None,
+                         choice_annotations_list = None,
                          debug: int = -1) -> Instance:
         # pylint: disable=arguments-differ
         fields: Dict[str, Field] = {}
@@ -258,11 +272,30 @@ class BertMCQAReader(DatasetReader):
         qa_fields = []
         segment_ids_fields = []
         qa_tokens_list = []
+        annotation_tags_fields = []
         for idx, choice in enumerate(choice_list):
             choice_context = context
+            choice_annotations = []
+            annotation_tags_array = None
+            if question_stem_annotations is not None:
+                choice_annotations = choice_annotations_list[idx]
             if choice_context_list is not None and choice_context_list[idx] is not None:
                 choice_context = choice_context_list[idx]
-            qa_tokens, segment_ids = self.bert_features_from_qa(question, choice, choice_context)
+                choice_annotations = []  # Not supported in examples yet
+            if question_stem_annotations is None:
+                qa_tokens, segment_ids = self.bert_features_from_qa(question, choice, choice_context)
+            else:
+                tmp = self.bert_features_from_qa_tags(question,
+                                                        choice,
+                                                        choice_context,
+                                                        context_annotations,
+                                                        question_stem_annotations,
+                                                        choice_annotations)
+                qa_tokens, segment_ids, annotations_tags = tmp
+                # After transpose has shape (num_annotation_tags, len(qa_tokens))
+                annotation_tags_array = numpy.array(annotations_tags).transpose()
+                annotation_tags_field = ArrayField(annotation_tags_array)
+                annotation_tags_fields.append(annotation_tags_field)
             qa_field = TextField(qa_tokens, self._token_indexers)
             segment_ids_field = SequenceLabelField(segment_ids, qa_field)
             qa_fields.append(qa_field)
@@ -271,6 +304,9 @@ class BertMCQAReader(DatasetReader):
             if debug > 0:
                 logger.info(f"qa_tokens = {qa_tokens}")
                 logger.info(f"segment_ids = {segment_ids}")
+                if annotation_tags_array is not None:
+                    logger.info(f"annotation_tags_array = {annotation_tags_array}")
+
 
         fields['question'] = ListField(qa_fields)
         fields['segment_ids'] = ListField(segment_ids_fields)
@@ -287,12 +323,36 @@ class BertMCQAReader(DatasetReader):
             # "choice_tokens_list": [[x.text for x in ct] for ct in choices_tokens_list],
         }
 
+        if len(annotation_tags_fields) > 0:
+            fields['annotation_tags'] = ListField(annotation_tags_fields)
+            metadata['annotation_tags'] = [x.array for x in annotation_tags_fields]
+
         if debug > 0:
             logger.info(f"answer_id = {answer_id}")
 
         fields["metadata"] = MetadataField(metadata)
 
         return Instance(fields)
+
+    # Returns list of (offset, label) for registered tags
+    def _get_tagged_spans(self, json):
+        if json is None:
+            return []
+        tagged_spans = json.get('tagged_spans')
+        if tagged_spans is None:
+            return []
+        res = []
+        for tagged_span in tagged_spans:
+            tag_label = self._normalize_tag(tagged_span['type'])
+            if tag_label is not None:
+                res.append((tagged_span['offset'], tag_label))
+        return res
+
+    def _normalize_tag(self, tag):
+        for tag_norm in self._annotation_tags:
+            if tag_norm in tag:
+                return tag_norm
+        return None
 
     @staticmethod
     def _truncate_tokens(tokens_a, tokens_b, max_length):
@@ -377,3 +437,51 @@ class BertMCQAReader(DatasetReader):
         segment_ids = list(itertools.repeat(0, len(question_tokens) + 2)) + \
                       list(itertools.repeat(1, len(choice_tokens) + 1))
         return tokens, segment_ids
+
+    def _get_tags(self, tokens, tag_offsets_labels):
+        tags = []
+        for token in tokens:
+            token_idx = token.idx
+            tag = [0] * self._num_annotation_tags
+            for offset, label in tag_offsets_labels:
+                if offset[0] <= token_idx < offset[1]:
+                    idx = self._annotation_tag_index[label]
+                    tag[idx] = 1
+            tags.append(tag)
+        return tags
+
+    def _tokens_and_tags(self, text, tag_offsets_labels):
+        word_tokens = self._tokenizer.tokenize(text)
+        tags_raw = self._get_tags(word_tokens, tag_offsets_labels)
+        tokens = []
+        tags = []
+        for wt, tag in zip(word_tokens, tags_raw):
+            wp_tokens = self._word_splitter.split_words(wt.text)
+            tags += [tag] * len(wp_tokens)
+            tokens += wp_tokens
+        return tokens, tags
+
+    def bert_features_from_qa_tags(self,
+                                   question: str,
+                                   answer: str,
+                                   context: str = None,
+                                   context_annotations = [],
+                                   question_stem_annotations = [],
+                                   choice_annotations = []):
+        cls_token = Token("[CLS]")
+        sep_token = Token("[SEP]")
+        empty_tags = [0] * self._num_annotation_tags
+        question_tokens, question_tags = self._tokens_and_tags(question, question_stem_annotations)
+        if context is not None:
+            context_tokens, context_tags = self._tokens_and_tags(context, context_annotations)
+            question_tokens = context_tokens + [sep_token] + question_tokens
+            question_tags = context_tags + [empty_tags] + question_tags
+        choice_tokens, choice_tags = self._tokens_and_tags(answer, choice_annotations)
+        question_tokens, choice_tokens = self._truncate_tokens(question_tokens, choice_tokens, self._max_pieces - 3)
+        question_tags = question_tags[-len(question_tokens):]
+        choice_tags = choice_tags[:len(choice_tokens)]
+        tokens = [cls_token] + question_tokens + [sep_token] + choice_tokens + [sep_token]
+        tags = [empty_tags] + question_tags + [empty_tags] + choice_tags + [empty_tags]
+        segment_ids = list(itertools.repeat(0, len(question_tokens) + 2)) + \
+                      list(itertools.repeat(1, len(choice_tokens) + 1))
+        return tokens, segment_ids, tags
--- allennlp/models/bert_models.py
+++ allennlp/models/bert_models.py
@@ -12,6 +12,7 @@ from allennlp.data import Vocabulary
 from allennlp.models.archival import load_archive
 from allennlp.models.model import Model
 from allennlp.modules import Attention
+from allennlp.modules.attention import MultiHeadAttention
 from allennlp.modules.scalar_mix import ScalarMix
 from allennlp.nn import RegularizerApplicator, util
 from allennlp.training.metrics import BooleanAccuracy, CategoricalAccuracy
@@ -199,6 +200,237 @@ class BertMCQAModel(Model):
             output_dict["loss"] = loss
 
         return output_dict
+
+    def get_metrics(self, reset: bool = False) -> Dict[str, float]:
+        return {
+            'accuracy': self._accuracy.get_metric(reset),
+        }
+
+    """
+    Needed to load old models into newer huggingface code:
+    """
+    @overrides
+    def load_state_dict(self, d):
+        tweaked_dict = {}
+        for k, v in d.items():
+            tweaked_k = k.replace('LayerNorm.gamma', 'LayerNorm.weight').replace('LayerNorm.beta', 'LayerNorm.bias')
+            tweaked_dict[tweaked_k] = v
+        super().load_state_dict(tweaked_dict)
+
+
+@Model.register("bert_mc_qa_annotations")
+class BertMCQAAnnotationsModel(Model):
+    """
+
+    """
+    def __init__(self,
+                 vocab: Vocabulary,
+                 pretrained_model: str = None,
+                 requires_grad: bool = True,
+                 top_layer_only: bool = True,
+                 bert_weights_model: str = None,
+                 per_choice_loss: bool = False,
+                 layer_freeze_regexes: List[str] = None,
+                 mc_strategy: str = None,
+                 num_annotations: int = None,
+                 attention_size: int = None,
+                 attention_normalize: bool = True,
+                 attention_loss_weight: float = 1.0,
+                 regularizer: Optional[RegularizerApplicator] = None) -> None:
+        super().__init__(vocab, regularizer)
+
+        if bert_weights_model:
+            logging.info(f"Loading BERT weights model from {bert_weights_model}")
+            bert_model_loaded = load_archive(bert_weights_model)
+            self._bert_model = bert_model_loaded.model._bert_model
+        else:
+            self._bert_model = BertModel.from_pretrained(pretrained_model)
+
+        for name, param in self._bert_model.named_parameters():
+            grad = requires_grad
+            if layer_freeze_regexes and grad:
+                grad = not any([bool(re.search(r, name)) for r in layer_freeze_regexes])
+            param.requires_grad = grad
+
+        bert_config = self._bert_model.config
+        self._output_dim = bert_config.hidden_size
+        self._dropout = torch.nn.Dropout(bert_config.hidden_dropout_prob)
+        self._per_choice_loss = per_choice_loss
+        classifier_input_dim = self._output_dim
+        classifier_output_dim = 1
+        self._pre_classifier = None
+        self._attention = None
+        self._attention_loss_weight = attention_loss_weight
+        self._num_annotations = num_annotations
+        if attention_size is not None and num_annotations is not None:
+            self._attention = MultiHeadAttention(num_heads=num_annotations,
+                                                 input_dim=self._output_dim,
+                                                 attention_dim=attention_size * num_annotations,
+                                                 values_dim=attention_size * num_annotations,
+                                                 normalize=attention_normalize)
+            classifier_input_dim *= 2
+        self._mc_strategy = mc_strategy
+        if self._mc_strategy in ["concat", "concat+"]:
+            classifier_input_dim *= 2
+            if self._mc_strategy == "concat+":
+                self._pre_classifier = Linear(classifier_input_dim, classifier_input_dim)
+                self._pre_classifier.apply(self._bert_model.init_bert_weights)
+        self._classifier = None
+        if bert_weights_model and hasattr(bert_model_loaded.model, "_classifier"):
+            self._classifier = bert_model_loaded.model._classifier
+            old_dims = (self._classifier.in_features, self._classifier.out_features)
+            new_dims = (classifier_input_dim, classifier_output_dim)
+            if old_dims != new_dims:
+                logging.info(f"NOT copying BERT classifier weights, incompatible dims: {old_dims} vs {new_dims}")
+                self._classifier = None
+        if self._classifier is None:
+            self._classifier = Linear(classifier_input_dim, classifier_output_dim)
+            self._classifier.apply(self._bert_model.init_bert_weights)
+        self._all_layers = not top_layer_only
+        if self._all_layers:
+            if bert_weights_model and hasattr(bert_model_loaded.model, "_scalar_mix") \
+                    and bert_model_loaded.model._scalar_mix is not None:
+                self._scalar_mix = bert_model_loaded.model._scalar_mix
+            else:
+                num_layers = bert_config.num_hidden_layers
+                initial_scalar_parameters = num_layers * [0.0]
+                initial_scalar_parameters[-1] = 5.0  # Starts with most mass on last layer
+                self._scalar_mix = ScalarMix(bert_config.num_hidden_layers,
+                                             initial_scalar_parameters=initial_scalar_parameters,
+                                             do_layer_norm=False)
+        else:
+            self._scalar_mix = None
+
+
+        if self._per_choice_loss:
+            self._accuracy = BooleanAccuracy()
+            self._loss = torch.nn.BCEWithLogitsLoss()
+        else:
+            self._accuracy = CategoricalAccuracy()
+            self._loss = torch.nn.CrossEntropyLoss()
+        self._debug = 2
+
+
+    def forward(self,
+                question: Dict[str, torch.LongTensor],
+                segment_ids: torch.LongTensor = None,
+                label: torch.LongTensor = None,
+                annotation_tags: torch.LongTensor = None,
+                metadata: List[Dict[str, Any]] = None) -> torch.Tensor:
+
+        self._debug -= 1
+        input_ids = question['tokens']
+
+        batch_size = input_ids.size(0)
+        num_choices = input_ids.size(1)
+
+        question_mask = (input_ids != 0).long()
+        question_mask_flat = util.combine_initial_dims(question_mask)
+        annotation_tags_flat = annotation_tags.view(batch_size * num_choices, self._num_annotations, -1)
+
+        if self._debug >= 0:
+            print(f"batch_size = {batch_size}")
+            print(f"num_choices = {num_choices}")
+            print(f"question_mask = {question_mask}")
+            print(f"input_ids.size() = {input_ids.size()}")
+            print(f"input_ids = {input_ids}")
+            print(f"segment_ids = {segment_ids}")
+            print(f"label = {label}")
+
+
+        encoded_layers, pooled_output = self._bert_model(input_ids=util.combine_initial_dims(input_ids),
+                                                         token_type_ids=util.combine_initial_dims(segment_ids),
+                                                         attention_mask=question_mask_flat,
+                                                         output_all_encoded_layers=self._all_layers)
+
+        if self._all_layers:
+            mixed_layer = self._scalar_mix(encoded_layers, question_mask)
+            pooled_output = self._bert_model.pooler(mixed_layer)
+
+        attention_loss = 0
+        attentions = None
+        if self._attention is not None:
+            query = encoded_layers[:,0]
+            attended_layer, attentions = self._attention(query, encoded_layers, question_mask_flat)
+            if self._debug >= 0:
+                print(f"attentions = {attentions}")
+                print(f"attention_tags = {annotation_tags}")
+                print(f"attention_tags_flat = {annotation_tags_flat}")
+            if annotation_tags is not None:
+                """
+                if not self._attention._normalize:
+                    # For now, if attention is not normalized we'll treat each attention as
+                    # prediction logit for 0/1 tags and average cross-entropy loss over unmasked tokens
+                    question_mask_float = question_mask.float()
+                    attention_loss = binary_cross_entropy_with_logits(
+                        attentions,
+                        tags.float(),
+                        weight=question_mask_float,
+                        reduction='sum'
+                    )/question_mask_float.sum()
+                    # Still need to normalize the attentions
+                    attentions = torch.sigmoid(attentions) * question_mask_float
+                    attentions = attentions / (attentions.sum(dim=1, keepdim=True) + 1e-13)
+                """
+                if True:
+                    # Take dot products of attentions and 0/1 tags to get combined probability for 1's
+                    tag_probs = (attentions * annotation_tags_flat).sum(dim=2) + 1e-10
+                    attention_loss = -tag_probs.log().mean()
+            if self._debug >= 0:
+                print(f"tag_probs = {tag_probs}")
+                print(f"attention_loss = {attention_loss}")
+
+            pooled_output = torch.cat((pooled_output, attended_layer), dim=1)
+
+
+        if self._mc_strategy in ["concat", "concat+"]:
+            # Average pooled output across all answer options and concat to original
+            pooled_avg = pooled_output.view(batch_size, num_choices, -1).mean(dim=1)
+            pooled_avg = torch.unsqueeze(pooled_avg, 1).repeat(1, num_choices, 1)
+            pooled_avg = pooled_avg.view(batch_size*num_choices, -1)
+            pooled_output = torch.cat((pooled_output, pooled_avg), dim=1)
+
+        if self._debug > 0:
+            print(f"pooled_output = {pooled_output}")
+
+        pooled_output = self._dropout(pooled_output)
+
+        if self._pre_classifier is not None:
+            pooled_output = gelu(pooled_output)
+            pooled_output = self._pre_classifier(pooled_output)
+            pooled_output = self._dropout(pooled_output)
+
+        label_logits = self._classifier(pooled_output)
+        label_logits_flat = label_logits.squeeze(1)
+        label_logits = label_logits.view(-1, num_choices)
+
+        output_dict = {}
+        output_dict['label_logits'] = label_logits
+        if self._per_choice_loss:
+            output_dict['label_probs'] = torch.sigmoid(label_logits_flat).view(-1, num_choices)
+            output_dict['answer_index'] = (label_logits_flat > 0).view(-1, num_choices)
+        else:
+            output_dict['label_probs'] = torch.nn.functional.softmax(label_logits, dim=1)
+            output_dict['answer_index'] = label_logits.argmax(1)
+
+        if label is not None:
+            if self._per_choice_loss:
+                binary_label = label.new_zeros((len(label), num_choices))
+                binary_label.scatter_(1, label.unsqueeze(1), 1.0)
+                binary_label = binary_label.view(-1,1).squeeze(1)
+                loss = self._loss(label_logits_flat, binary_label.float())
+                self._accuracy(label_logits_flat > 0, binary_label.byte())
+            else:
+                loss = self._loss(label_logits, label)
+                self._accuracy(label_logits, label)
+            if attention_loss != 0:
+                loss += self._attention_loss_weight * attention_loss
+            output_dict["loss"] = loss
+
+        if self._debug > 0:
+            print(output_dict)
+        return output_dict
+
 
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
         return {
